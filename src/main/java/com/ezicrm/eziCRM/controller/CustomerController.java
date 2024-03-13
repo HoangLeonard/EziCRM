e620c47f0844e29a0fd49a701c6b82277d81fcec
hoang minh <hnm08072002@gmail.com>
2024-03-13 09:36:13 +0700

    add controller

---
+++ src/main/java/com/ezicrm/eziCRM/controller/CustomerController.java
@@ -0,0 +1,28 @@
+package com.ezicrm.eziCRM.controller;
+
+import com.ezicrm.eziCRM.model.CustomerEntity;
+import com.ezicrm.eziCRM.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/api/v1/Customers")
+public class CustomerController {
+
+    private final CustomerRepository repository;
+
+    public CustomerController(CustomerRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("")
+    List<CustomerEntity> getAllCustomers() {
+        System.out.println("aaaa");
+        return repository.findAll();
+    }
+
+}
