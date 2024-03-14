3657d554768e085e79b3671a084f20a1c889026c
Chung Hòa <chungminhhoa1998@gmail.com>
2024-03-14 12:34:52 +0700

    api category, group category

--- src/main/java/com/ezicrm/eziCRM/controller/GroupCategoryController.java
+++ src/main/java/com/ezicrm/eziCRM/controller/GroupCategoryController.java
@@ -1,4 +1,76 @@
 package com.ezicrm.eziCRM.controller;
 
+import com.ezicrm.eziCRM.model.GroupCategoryEntity;
+import com.ezicrm.eziCRM.model.ResponseDTO;
+import com.ezicrm.eziCRM.repository.GroupCategoryRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.Timestamp;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/v1/GroupCategory")
 public class GroupCategoryController {
+    private final GroupCategoryRepository repository;
+    public GroupCategoryController(GroupCategoryRepository repository)
+    {
+        this.repository = repository;
+    }
+
+    @GetMapping("")
+    ResponseEntity<ResponseDTO> getAllGroupCategories(){
+        List<GroupCategoryEntity> groupCategoryEntities = repository.findAll();
+
+        return !groupCategoryEntities.isEmpty() ?
+                ResponseEntity.status(HttpStatus.OK).body(
+                        new ResponseDTO("oke", "Successfully query GC", groupCategoryEntities)
+                ):
+                ResponseEntity.status(HttpStatus.OK).body(
+                    new ResponseDTO("oke", "No GC found", groupCategoryEntities)
+                );
+    }
+
+    @GetMapping("/{id}")
+    ResponseEntity<ResponseDTO> getGroupCategoryById(@PathVariable Long id){
+        Optional<GroupCategoryEntity> groupCategoryEntity = repository.findById(id);
+        return groupCategoryEntity.isPresent()?
+                ResponseEntity.status(HttpStatus.OK).body(
+                        new ResponseDTO("oke", "successfully query category.", groupCategoryEntity)
+                ):
+                ResponseEntity.status(HttpStatus.OK).body(
+                        new ResponseDTO("oke", "no group category found.", groupCategoryEntity)
+                );
+    }
+
+    @PostMapping("/insert")
+    ResponseEntity<ResponseDTO> insertCategory(@RequestBody GroupCategoryEntity groupCategory){
+        boolean checkGroupCategory = repository.existsByGroupName(groupCategory.getGroupName());
+        if(!checkGroupCategory){
+            return ResponseEntity.status(HttpStatus.OK).body(
+                    new ResponseDTO("ok", "Insert successfully", repository.save(groupCategory))
+            );
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(
+                new ResponseDTO("Error", "Error", "")
+        );
+    }
+
+    @DeleteMapping("/{id}")
+    ResponseEntity<ResponseDTO> deleteGroupCategory(@PathVariable Long id){
+        boolean groupCategory = repository.existsById(id);
+
+        if(groupCategory){
+            repository.deleteById(id);
+            return ResponseEntity.status(HttpStatus.OK).body(
+                    new ResponseDTO("ok", "successfully query group category.","")
+            );
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(
+                new ResponseDTO("oke", "no group category found.", "")
+        );
+    }
 }
--- src/main/java/com/ezicrm/eziCRM/repository/CategoryRepository.java
+++ src/main/java/com/ezicrm/eziCRM/repository/CategoryRepository.java
@@ -4,4 +4,5 @@ import com.ezicrm.eziCRM.model.CategoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<CategoryEntity, Long> {
+     boolean existsByLabelName(String labelName);
 }
